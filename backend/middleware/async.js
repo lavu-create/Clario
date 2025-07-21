c52bf8285be4369bb1bfd8cf6994ba100f53e98a
Ꭿkhil <theakhil2000@gmail.com>
2025-07-21 19:21:07 +0530

    Delete backend directory

--- backend/middleware/async.js
+++
@@ -1,6 +0,0 @@
-// Async handler to wrap around async/await functions to handle try/catch
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
